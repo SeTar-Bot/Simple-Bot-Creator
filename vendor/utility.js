7603daf3334a6fa5f3b8cbe5c560ff6ab643af96
EhsanFox <EhsanFox@users.noreply.github.com>
2021-04-11 09:11:00 -0700

    new TokenValidator

--- vendor/utility.js
+++ vendor/utility.js
@@ -1,3 +1,4 @@
+const fetch = require('node-fetch');
 module.exports = {
     pad: function pad(n, z) {
         z = z || 2;
@@ -16,16 +17,38 @@ module.exports = {
         var hrs = (s - mins) / 60;
         return module.exports.pad(hrs) + ':' + module.exports.pad(mins) + ':' + module.exports.pad(secs);
       },
-    formatBytes: function(bytes, decimals = 2) {
-    if (bytes === 0) return '0 Bytes';
+      formatBytes: function(bytes, decimals = 2) {
+        if (bytes === 0) return '0 Bytes';
 
-    const k = 1024;
-    const dm = decimals < 0 ? 0 : decimals;
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+        const k = 1024;
+        const dm = decimals < 0 ? 0 : decimals;
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
+        const i = Math.floor(Math.log(bytes) / Math.log(k));
 
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];   
-}
-
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];   
+      },
+      TokenValidator: function(token)
+      {
+        fetch('https://discord.com/api/v6/users/@me', {
+            method: 'GET',
+            headers: { 'Authorization': 'Bot ' + token  }
+        })
+        .then(res => res.json())
+        .then(data => {
+            const errorCodes = [0, 400, 401, 50035];
+            if(data.code && errorCodes.includes(data.code))
+            {
+                return { result: false, message: data.message };
+            }
+            else
+            {
+                return { 
+                    result: true,
+                    data.id,
+                    data.username,
+                    tag: data.username + data.discriminator
+            }
+        }); 
+      }
 };
