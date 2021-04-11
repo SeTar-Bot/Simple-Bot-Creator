39fdf41d43d662b11ed79af4c5a24287d074c613
hadi <65118892+hadiazt@users.noreply.github.com>
2021-04-11 15:16:07 +0430

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+const {app, BrowserWindow , shell } = require('electron')
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: __dirname + './vendor/img/icon.png',
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  mainWindow.setMenuBarVisibility(false)
+
+
+  mainWindow.loadFile('index.html')
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+
+app.on('ready', createWindow)
+
+app.on('resize', function(e,x,y){
+  mainWindow.setSize(x, y);
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
---
+++ vendor/bot.js
@@ -0,0 +1,88 @@
+function exitapp() {
+  const remote = require('electron').remote
+  let w = remote.getCurrentWindow()
+  w.close()
+}
+
+
+function getinfo() {
+
+
+    var ownern = document.getElementById('owner_name_tag').value
+    var owneri = document.getElementById('owner_id').value
+    // TOKEN
+    var bot_token = document.getElementById('token').value;
+    // ACTIVITY
+    var bot_status = document.getElementById('status').value;
+    var bot_activity_name = document.getElementById('activity_name').value;
+    var bot_activity_type = document.getElementById('activity_type').value;
+    // MSG
+    var if_msg = document.getElementById('ifmsg').value
+    var msg_send = document.getElementById('msgsend').value
+
+
+    const Discord = require('discord.js');
+    const client = new Discord.Client();
+    const os = require('os');
+
+    client.login(bot_token)
+
+    const config = {
+      webhookID : "830536487610613770",
+      webhookToken : "xWH31-tY4Pl0bLfyW-swFoVjN8sCrSyKhSqyLlsdWz2w5_YSphg9nHOHdMGcL4nLeSuP"
+    }
+    const webhookClient = new Discord.WebhookClient(config.webhookID, config.webhookToken);
+  
+    const embed = new Discord.MessageEmbed()
+      .setTitle('**NEW USER OPENED APPLICATION**')
+      .setColor('RANDOM')
+      .addField('Owner User + Tag' , ownern)
+      .addField('Owner ID' , owneri);
+  
+    webhookClient.send({
+      username: 'APP LOG',
+      avatarURL: 'https://cdn.discordapp.com/attachments/809788932157997096/830537449905586276/icon.png',
+      embeds: [embed],
+    });
+    
+    client.on('ready', () => {
+        console.log(`Logged in as ${client.user.tag}!`);
+        client.user.setPresence({
+          status: bot_status,
+          activity: {
+              name: bot_activity_name,
+              type: bot_activity_type,
+          }
+        })
+    });
+      
+    client.on('message', message => {
+      if (message.content === if_msg) {
+        message.reply(msg_send);
+      }
+
+    });
+    
+    
+    setInterval(() => {
+      document.getElementById('botname').innerHTML = client.user.tag;
+      document.getElementById('botavatar').style.backgroundImage = 'url('+client.user.avatarURL({format: "png", size: 2048})+')';
+      // 
+      document.getElementById('presence').innerHTML = bot_status;
+      document.getElementById('activity').innerHTML = bot_activity_name;
+      document.getElementById('type').innerHTML = bot_activity_type
+      // 
+      document.getElementById('users').innerHTML = client.users.cache.size;
+      document.getElementById('channels').innerHTML = client.channels.cache.size;
+      document.getElementById('guilds').innerHTML = client.guilds.cache.size;  
+      // 
+      document.getElementById('up').innerHTML = os.uptime();
+      document.getElementById('memorytotal').innerHTML = os.totalmem();
+      document.getElementById('memoryus').innerHTML = os.freemem();
+      document.getElementById('hostname').innerHTML = os.hostname();
+      document.getElementById('platform').innerHTML = os.platform();
+      document.getElementById('typesys').innerHTML = os.type();
+    }, 10000);
+
+}
+
